c20bdebee7d43357dc712ac7e35b3427bf2f5a5d
Tariquzzaman-faisal <tariquzzamanfaisal1964@gmail.com>
2021-02-23 01:04:18 +0600

    game works but animation needs to be slowed down

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
-//variables
 var colors = ["red", "green", "blue", "yellow"];
+var player = [];
 var string = [];
-var player = "nan";
-//Game Started
 var gamestart = false;
-
+var stage = 1;
+var taken = 0;
+//Listerners
 $(document).keypress(function(){
   if(!gamestart){
-    pushSequence();
     gamestart = true;
-    PlaySound(string[0]);
+    nextSequence();
+    $("#level-title").text("level " + stage);
   }
-})
-//Push next Sequence
-//PlaySound
-function PlaySound(id){
+});
+
+$(".btn").click(function(){
+  var id = $(this).attr("id");
+  player.push(id);
+  playSound(id);
+  animate("#" + id);
+  taken++;
+  if(taken == stage){
+    taken = 0;
+    check();
+
+  }
+}); 
+
+//functions
+function nextSequence(){
+  reset();
+  for(var i = 0; i < stage; i++){
+    var rnd = Math.floor(Math.random()*4);
+    var color = colors[rnd];
+    console.log(color);
+    string.push(color);
+    $("#" + color).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(color);
+  }
+ }
+
+function playSound(id){
   var sfx = new Audio("sounds/" + id + ".mp3");
   sfx.play();
-  Animate("#" + id);
 }
-//Animate
-function Animate(id){
+
+function animate(id){
   $(id).addClass("pressed");
   setTimeout(function(){
     $(id).removeClass("pressed");
   }, 100);
 }
-//Push Player Sequence
-function pushSequence(){
-  string.push(colors[Math.floor(Math.random()*4)]);
+
+function check(){
+  var valid = 1;
+  for(var i = 0; i < string.length; i++){
+    if(player[i] != string[i]) valid = 0;
+  }
+  if(valid){
+    setTimeout(function(){
+      nextSequence();
+    }, 1000);
+    stage = stage + 1;
+    $("#level-title").text("level " + stage);
+  }
+  else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    reset();
+    stage = 1;
+  }
 }
-//Check result
-//Gameover
+
+function reset(){
+  string = [];
+  player = [];
+  gamestart = false;
+}
